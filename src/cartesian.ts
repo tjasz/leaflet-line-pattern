201ea46f518437cdc6a03c2c0bf1837127ede87d
tjasz <nukorausa@hotmail.com>
2024-06-21 12:02:04 -0700

    Add doc comments, rework cartesian helpers

--- src/cartesian.ts
+++ src/cartesian.ts
@@ -1,34 +1,77 @@
 export type Point = { x: number; y: number };
 
+/**
+ * @param x 
+ * @returns The square of a number `x`: (x*x).
+ */
 function square(x: number) {
   return x * x;
 }
 
-function L2norm(x: number, y: number) {
-  return Math.sqrt(square(x) + square(y));
+/**
+ * @param p
+ * @returns The L2 norm of a point `p`: the Euclidean distance to the origin.
+ */
+function L2norm(p: Point) {
+  return Math.sqrt(square(p.x) + square(p.y));
 }
 
+/**
+ * @param p1 
+ * @param p2 
+ * @returns The Euclidean distance between two points `p1` and `p2`.
+ */
 export function dist(p1: Point, p2: Point) {
-  return L2norm(p1.x - p2.x, p1.y - p2.y);
+  return L2norm({ x: p1.x - p2.x, y: p1.y - p2.y });
 }
 
+/**
+ * Get the bearing in radians from point `p1` to point `p2`.
+ * @param p1 The origin point
+ * @param p2 The destination point
+ * @returns The bearing in radians from point `p1` to point `p2`
+ */
+export function bearing(p1: Point, p2: Point) {
+  return Math.atan2(p2.y - p1.y, p2.x - p2.x);
+}
+
+/**
+ * Return the point that results from moving an initial point along a bearing
+ * for a distance.
+ * @param initialPoint The initial point
+ * @param distance The distance along the bearing to move
+ * @param bearingRadians The bearing in radians along which to move
+ * @returns The point that is the result of moving `p` by `dist` units along `bearingRadians`.
+ */
 export function moveAlongBearing(
-  p: Point,
-  dist: number,
+  initialPoint: Point,
+  distance: number,
   bearingRadians: number
 ): Point {
   return {
-    x: p.x + dist * Math.cos(bearingRadians),
-    y: p.y + dist * Math.sin(bearingRadians),
+    x: initialPoint.x + distance * Math.cos(bearingRadians),
+    y: initialPoint.y + distance * Math.sin(bearingRadians),
   }
 }
 
-export function rotateAroundOrigin(p: Point, radians: number): Point {
-  const magnitude = L2norm(p.x, p.y);
-  const originalBearing = Math.atan2(p.y, p.x);
+/**
+ * Return the point that results from rotating an initial point
+ * for a given amount around an axis point.
+ * @param initialPoint The initial point
+ * @param rotationRadians The amount to rotate around the axis
+ * @param axisPoint The point to rotate around (defaults to the origin 0,0)
+ * @returns 
+ */
+export function rotateAroundOrigin(
+  initialPoint: Point,
+  rotationRadians: number,
+  axisPoint: Point = { x: 0, y: 0 },
+): Point {
+  const magnitude = dist(axisPoint, initialPoint);
+  const originalBearing = bearing(axisPoint, initialPoint);
   const result: Point = {
-    x: magnitude * Math.cos(originalBearing + radians),
-    y: magnitude * Math.sin(originalBearing + radians),
+    x: axisPoint.x + magnitude * Math.cos(originalBearing + rotationRadians),
+    y: axisPoint.y + magnitude * Math.sin(originalBearing + rotationRadians),
   };
   return result;
 }
