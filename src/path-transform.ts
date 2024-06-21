2957adf46bbe81ec705c6394dba4ecde39a7bc25
tjasz <nukorausa@hotmail.com>
2024-06-21 14:26:44 -0700

    Add translate function to path transform

--- src/path-transform.ts
+++ src/path-transform.ts
@@ -1,14 +1,71 @@
 import { SvgPath } from "./svg-path"
 
-export function round(p: SvgPath, places: number): SvgPath {
+/**
+ * Round all the coordinates in a path to the number of places given.
+ * @param path The input path
+ * @param places The number of decimal places to round to
+ * @returns A copy of the path with the coordinates rounded
+ */
+// TODO is applying the rounding to all parameters a problem for arcs?
+export function round(path: SvgPath, places: number): SvgPath {
   const round = (n: number) => +n.toFixed(places);
-  const result = p.map(c => {
+  const result = path.map(c => {
     return { ...c, parameters: c.parameters.map(round) }
   });
   return result;
 }
 
+/**
+ * Translate a given path horizontally by `dx` and vertically by `dy`.
+ * @param path The input path.
+ * @param dx The amount to move the path along the X axis
+ * @param dy The amount to move the path along the Y axis
+ * @returns A copy of the path translated by (`dx`, `dy`)
+ */
+export function translate(path: SvgPath, dx: number, dy: number): SvgPath {
+  if (dx === 0 && dy === 0) {
+    return path;
+  }
+  const result = path.map(c => {
+    if (c.isAbsolute) {
+      switch (c.operator) {
+        // commands with x and y coordinates
+        case "M":
+        case "L":
+        case "C":
+        case "S":
+        case "Q":
+        case "T":
+          return {
+            ...c, parameters: c.parameters.map((v, i) => v + (i % 2 ? dy : dx))
+          }
+        // commands with just x coordinates
+        case "H":
+          return { ...c, parameters: c.parameters.map(x => x + dx) }
+        // commands with just y coordinates
+        case "V":
+          return { ...c, parameters: c.parameters.map(y => y + dy) }
+        // commands with no coordinates
+        case "Z":
+          return c;
+        // the arc command
+        case "A":
+          // each arc is defined with 7 parameters where the last two are X and Y
+          return {
+            ...c, parameters: c.parameters.map((v, i) => v + (i % 7 === 5 ? dx : i % 7 === 6 ? dy : 0))
+          }
+        default:
+          throw new Error("Invalid SVG command: " + c.operator)
+      }
+    } else {
+      return c;
+    }
+  });
+  return result;
+}
+
 const Transform = {
-  round
+  round,
+  translate
 }
 export default Transform;
--- test/path-transform.test.ts
+++ test/path-transform.test.ts
@@ -39,3 +39,54 @@ describe("translate", () => {
     }
   )
 })
+
+describe("translate", () => {
+  it.each<[[SvgPath, number, number], SvgPath]>([
+    // The empty path translated by any amount is the empty path
+    [[[], 0, 0], []],
+    [[[], -10, -10], []],
+    [[[], -10, 10], []],
+    [[[], 10, -10], []],
+    [[[], 10, 10], []],
+    // any path from any quadrant translated by 0 is the same
+    [[[{ isAbsolute: true, operator: CommandOperator.Move, parameters: [-1, -2] }], 0, 0], [{ isAbsolute: true, operator: CommandOperator.Move, parameters: [-1, -2] }]],
+    [[[{ isAbsolute: true, operator: CommandOperator.Move, parameters: [-1, 2] }], 0, 0], [{ isAbsolute: true, operator: CommandOperator.Move, parameters: [-1, 2] }]],
+    [[[{ isAbsolute: true, operator: CommandOperator.Move, parameters: [1, -2] }], 0, 0], [{ isAbsolute: true, operator: CommandOperator.Move, parameters: [1, -2] }]],
+    [[[{ isAbsolute: true, operator: CommandOperator.Move, parameters: [1, 2] }], 0, 0], [{ isAbsolute: true, operator: CommandOperator.Move, parameters: [1, 2] }]],
+    // translate into each quadrant
+    [[[{ isAbsolute: true, operator: CommandOperator.Move, parameters: [1, 2, 3, 4] }], -10, -10], [{ isAbsolute: true, operator: CommandOperator.Move, parameters: [-9, -8, -7, -6] }]],
+    [[[{ isAbsolute: true, operator: CommandOperator.Move, parameters: [1, 2, 3, 4] }], -10, 10], [{ isAbsolute: true, operator: CommandOperator.Move, parameters: [-9, 12, -7, 14] }]],
+    [[[{ isAbsolute: true, operator: CommandOperator.Move, parameters: [1, 2, 3, 4] }], 10, -10], [{ isAbsolute: true, operator: CommandOperator.Move, parameters: [11, -8, 13, -6] }]],
+    [[[{ isAbsolute: true, operator: CommandOperator.Move, parameters: [1, 2, 3, 4] }], 10, 10], [{ isAbsolute: true, operator: CommandOperator.Move, parameters: [11, 12, 13, 14] }]],
+    // // translate an L command
+    // [[[{ operator: CommandOperator.Move, parameters: [1, 2] }, { operator: "L", parameters: [3, 4, 5, 6] }], 10, 20], [{ operator: CommandOperator.Move, parameters: [11, 22] }, { operator: "L", parameters: [13, 24, 15, 26] }]],
+    // // translate an H command
+    // [[[{ operator: CommandOperator.Move, parameters: [1, 2] }, { operator: "H", parameters: [3, 4] }], 10, 20], [{ operator: CommandOperator.Move, parameters: [11, 22] }, { operator: "H", parameters: [13, 14] }]],
+    // // translate a V command
+    // [[[{ operator: CommandOperator.Move, parameters: [1, 2] }, { operator: "V", parameters: [3, 4] }], 10, 20], [{ operator: CommandOperator.Move, parameters: [11, 22] }, { operator: "V", parameters: [23, 24] }]],
+    // // translate a C command
+    // [[[{ operator: CommandOperator.Move, parameters: [1, 2] }, { operator: "C", parameters: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] }], 10, 20], [{ operator: CommandOperator.Move, parameters: [11, 22] }, { operator: "C", parameters: [13, 24, 15, 26, 17, 28, 19, 30, 21, 32, 23, 34] }]],
+    // // translate a S command
+    // [[[{ operator: CommandOperator.Move, parameters: [1, 2] }, { operator: "S", parameters: [3, 4, 5, 6, 7, 8, 9, 10] }], 10, 20], [{ operator: CommandOperator.Move, parameters: [11, 22] }, { operator: "S", parameters: [13, 24, 15, 26, 17, 28, 19, 30] }]],
+    // // translate a Q command
+    // [[[{ operator: CommandOperator.Move, parameters: [1, 2] }, { operator: "Q", parameters: [3, 4, 5, 6, 7, 8, 9, 10] }], 10, 20], [{ operator: CommandOperator.Move, parameters: [11, 22] }, { operator: "Q", parameters: [13, 24, 15, 26, 17, 28, 19, 30] }]],
+    // // translate a T command
+    // [[[{ operator: CommandOperator.Move, parameters: [1, 2] }, { operator: "T", parameters: [3, 4, 5, 6] }], 10, 20], [{ operator: CommandOperator.Move, parameters: [11, 22] }, { operator: "T", parameters: [13, 24, 15, 26] }]],
+    // // translate a A command
+    // // only the last 2 of 7 parameters should be translated
+    // [[[{ operator: CommandOperator.Move, parameters: [1, 2] }, { operator: "A", parameters: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] }], 10, 20], [{ operator: CommandOperator.Move, parameters: [11, 22] }, { operator: "A", parameters: [3, 4, 5, 6, 7, 18, 29, 10, 11, 12, 13, 14, 25, 36] }]],
+    // // translate a Z command
+    // [[[{ operator: CommandOperator.Move, parameters: [1, 2] }, { operator: "Z", parameters: [] }], 10, 20], [{ operator: CommandOperator.Move, parameters: [11, 22] }, { operator: "Z", parameters: [] }]],
+    // // test that relative commands do not change
+    // // TODO although, should a leading "m" change??
+    // [[[{ operator: "m", parameters: [1, 2] }, { operator: "l", parameters: [3, 4] }, { operator: "h", parameters: [5] }], 10, 20], [{ operator: "m", parameters: [1, 2] }, { operator: "l", parameters: [3, 4] }, { operator: "h", parameters: [5] }]],
+    // [[[{ operator: "m", parameters: [1, 2] }, { operator: "v", parameters: [3] }, { operator: "c", parameters: [4, 5, 6, 7, 8, 9] }], 10, 20], [{ operator: "m", parameters: [1, 2] }, { operator: "v", parameters: [3] }, { operator: "c", parameters: [4, 5, 6, 7, 8, 9] }]],
+    // [[[{ operator: "m", parameters: [1, 2] }, { operator: "s", parameters: [3, 4, 5, 6] }, { operator: "q", parameters: [7, 8, 9, 10] }], 10, 20], [{ operator: "m", parameters: [1, 2] }, { operator: "s", parameters: [3, 4, 5, 6] }, { operator: "q", parameters: [7, 8, 9, 10] }]],
+    // [[[{ operator: "m", parameters: [1, 2] }, { operator: "t", parameters: [3, 4] }, { operator: "z", parameters: [] }], 10, 20], [{ operator: "m", parameters: [1, 2] }, { operator: "t", parameters: [3, 4] }, { operator: "z", parameters: [] }]],
+  ])(
+    "translate(%p)",
+    (args: [SvgPath, number, number], expected: SvgPath) => {
+      expect(Transform.translate(...args)).toEqual(expected);
+    }
+  )
+})
