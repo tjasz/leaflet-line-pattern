8b9a7b337acf46a19e3d90ad98d559bcef0007b1
tjasz <nukorausa@hotmail.com>
2024-06-22 10:50:41 -0700

    Add an example feature

--- demo/index.js
+++ demo/index.js
@@ -4,6 +4,24 @@ const osmTiles = new L.TileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}
 
 const map = new L.Map("map", {
   layers: [osmTiles],
-  center: [47.5, -122.3],
+  center: [45, -125],
   zoom: 10,
 })
+
+const features = new L.GeoJSON({
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "LineString",
+        coordinates: [
+          [-125, 45],
+          [-125.1, 45],
+          [-125.2, 45.1],
+          [-125.2, 45],
+        ]
+      }
+    }
+  ]
+}).addTo(map);
